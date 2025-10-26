2a0f0bacd0e0fed04dd1066d1c7bf7d36dfcef75
Hung <hung.do@fau.de>
2025-10-26 21:04:46 +0100

    Implemented B01

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -6,9 +6,9 @@ export const ESCAPE_CHARACTER = '\\';
  * Special characters within the string may need masking, if they are to appear verbatim.
  * There are only two special characters, the delimiter character and the escape character.
  * The escape character can't be set, the delimiter character can.
- * 
+ *
  * Homogenous name examples
- * 
+ *
  * "oss.cs.fau.de" is a name with four name components and the delimiter character '.'.
  * "///" is a name with four empty components and the delimiter character '/'.
  * "Oh\.\.\." is a name with one component, if the delimiter character is '.'.
@@ -20,7 +20,10 @@ export class Name {
 
     /** Expects that all Name components are properly masked */
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+        this.components = other;
+        if (delimiter) {
+            this.delimiter = delimiter;
+        }
     }
 
     /**
@@ -29,45 +32,76 @@ export class Name {
      * Users can vary the delimiter character to be used
      */
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        let result: string = "";
+        for (let i = 0; i < this.components.length; i++) {
+            let component: string = this.components[i];
+            component = component.replace(new RegExp(this.generateRegex(ESCAPE_CHARACTER + this.delimiter), 'g'), this.delimiter);
+            console.log(component);
+            component = component.replace(new RegExp(this.generateRegex(ESCAPE_CHARACTER + ESCAPE_CHARACTER), 'g'), ESCAPE_CHARACTER);
+            console.log(component);
+            result += component;
+            if (i < this.components.length - 1) {
+                result += delimiter;
+            }
+        }
+        return result;
     }
 
-    /** 
+    private generateRegex(s: string): string {
+        let escapedString: string = s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+        return escapedString;
+    }
+
+    /**
      * Returns a machine-readable representation of Name instance using default special characters
      * Machine-readable means that from a data string, a Name can be parsed back in
      * The special characters in the data string are the default characters
      */
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        let result: string = "";
+        if(this.delimiter == DEFAULT_DELIMITER) {
+            result = this.components.join(this.delimiter);
+        }
+        else {
+            for (let i = 0; i < this.components.length; i++) {
+                let component: string = this.components[i];
+                component = component.replace(new RegExp(this.generateRegex(ESCAPE_CHARACTER + this.delimiter), 'g'), this.delimiter);
+                component = component.replace(new RegExp(this.generateRegex(DEFAULT_DELIMITER), 'g'), ESCAPE_CHARACTER + DEFAULT_DELIMITER);
+                result += component;
+                if (i < this.components.length - 1) {
+                    result += DEFAULT_DELIMITER;
+                }
+            }
+        }
+        return result;
     }
 
     /** Returns properly masked component string */
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        return this.components[i];
     }
 
     /** Expects that new Name component c is properly masked */
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components[i] = c;
     }
 
-     /** Returns number of components in Name instance */
-     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+    /** Returns number of components in Name instance */
+    public getNoComponents(): number {
+        return this.components.length;
     }
 
     /** Expects that new Name component c is properly masked */
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components.splice(i, 0, c);
     }
 
     /** Expects that new Name component c is properly masked */
     public append(c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components.push(c);
     }
 
     public remove(i: number): void {
-        throw new Error("needs implementation or deletion");
+        this.components.splice(i, 1);
     }
-
 }
--- test/adap-b01/names/Name.test.ts
+++ test/adap-b01/names/Name.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { Name } from "../../../src/adap-b01/names/Name";
+import {ESCAPE_CHARACTER, Name} from "../../../src/adap-b01/names/Name";
 
 describe("Basic initialization tests", () => {
   it("test construction 1", () => {
@@ -32,4 +32,51 @@ describe("Escape character extravaganza", () => {
     n.append("people");
     expect(n.asString()).toBe("oss.cs.fau.de#people");
   });
+});
+
+describe("Test123", () => {
+    it("Test1", () => {
+        // Original name string = "oss.cs.fau.de"
+        let test: string = "\\ \\ \\ \\ \a";
+        console.log(test);
+        test = test.replace(new RegExp(ESCAPE_CHARACTER + 'a', 'g'), 'a');
+        console.log(test);
+        test = test.replace(new RegExp(ESCAPE_CHARACTER + ESCAPE_CHARACTER, 'g'), ESCAPE_CHARACTER);
+        console.log(test);
+    });
+
+    it("insert", () => {
+        let n: string[] = ["oss", "fau", "de"];
+        let i: number = 1;
+        let c: string = "cs";
+        n.splice(i, 0, c);
+        console.log(n);
+    })
+
+    it("remove", () => {
+        let n: string[] = ["oss", "cs", "fau", "de"];
+        let i: number = 3;
+        n.splice(i, 1);
+        console.log(n);
+    })
+    it("asDataString", () => {
+        let n: Name = new Name(["oss\.cs\.fau\.de","people"], '#');
+        console.log(n.asDataString());
+        expect(n.asDataString(), "oss\\.cs\\.fau\\.de.people")
+    })
+    it("QS", () => {
+        let n: Name = new Name(["Oh\.\.\.","Oh\.\.\."], '.');
+        console.log(n.asString());
+        let s = "Oh\\.";
+        console.log(JSON.stringify(s));
+    })
+    it("asString", () => {
+        let n : Name = new Name(["a\\.\\.", "b\\\\"], '.');
+        console.log(n.asString());
+    })
+
+    it('asDataString 2', () => {
+        let n : Name = new Name (["f.a\\u\\#", "oss\\#de"], '#');
+        console.log(n.asDataString());
+    });
 });
