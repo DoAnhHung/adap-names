d2c71bc62e79be95b610c9028ab68cb427033290
Hung <hung.do@fau.de>
2025-10-27 14:58:39 +0100

    Added MethodTypes

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -31,6 +31,7 @@ export class Name {
      * Special characters are not escaped (creating a human-readable string)
      * Users can vary the delimiter character to be used
      */
+    // @methodtype conversion-method
     public asString(delimiter: string = this.delimiter): string {
         let result: string = "";
         for (let i = 0; i < this.components.length; i++) {
@@ -55,6 +56,7 @@ export class Name {
      * Machine-readable means that from a data string, a Name can be parsed back in
      * The special characters in the data string are the default characters
      */
+    // @methodtype conversion-method
     public asDataString(): string {
         let result: string = "";
         if(this.delimiter == DEFAULT_DELIMITER) {
@@ -75,30 +77,36 @@ export class Name {
     }
 
     /** Returns properly masked component string */
+    // @methodtype get-method
     public getComponent(i: number): string {
         return this.components[i];
     }
 
     /** Expects that new Name component c is properly masked */
+    // @methodtype set-method
     public setComponent(i: number, c: string): void {
         this.components[i] = c;
     }
 
     /** Returns number of components in Name instance */
+    // @methodtype get-method
     public getNoComponents(): number {
         return this.components.length;
     }
 
     /** Expects that new Name component c is properly masked */
+    // @methodtype command-method
     public insert(i: number, c: string): void {
         this.components.splice(i, 0, c);
     }
 
     /** Expects that new Name component c is properly masked */
+    // @methodtype command-method
     public append(c: string): void {
         this.components.push(c);
     }
 
+    // @methodtype command-method
     public remove(i: number): void {
         this.components.splice(i, 1);
     }
